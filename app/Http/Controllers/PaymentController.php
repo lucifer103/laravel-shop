bddd2b627fae6780559abbe38df14ac0df15e9bf
Lucifer <hug.m@foxmail.com>
2020-02-13 11:26:17 +0800

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InvalidRequestException;
 use Endroid\QrCode\QrCode;
+use App\Events\OrderPaid;
 
 class PaymentController extends Controller
 {
@@ -66,6 +67,9 @@ class PaymentController extends Controller
             'payment_method' => 'alipay',    // 支付方式
             'payment_no' => $data->trade_no // 支付宝订单号
         ]);
+
+        $this->afterPaid($order);
+
         return app('alipay')->success();
     }
 
@@ -118,6 +122,13 @@ class PaymentController extends Controller
             'payment_no' => $data->transaction_id,
         ]);
 
+        $this->afterPaid($order);
+
         return app('wechat_pay')->success();
+    }
+
+    public function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
