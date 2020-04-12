d6ab8ca6e8cc64aa415920ef028a43f60574c1d7
Lucifer <hug.m@foxmail.com>
2020-04-12 18:24:54 +0800

    ✨feat(installment): create installments

--- config/app.php
+++ config/app.php
@@ -128,6 +128,17 @@ return [
 
     'ngrok_url' => env('NGROK_URL'),
 
+    // 分期费率，key 为期数，value 为费率
+    'installment_fee_rate' => [
+        3 => 1.5,
+        6 => 2,
+        12 => 2.5,
+    ],
+    // 最低分期金额
+    'min_installment_amount' => 300,
+    // 逾期日息 0.05%
+    'installment_fine_rate' => 0.05,
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
