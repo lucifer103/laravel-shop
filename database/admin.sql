bded681dc473490badae3966a47dcd32bd7a2f22
Lucifer <hug.m@foxmail.com>
2020-04-17 03:33:27 +0800

    ✨feat(seckill): seckill product manages the background

--- database/admin.sql
+++ database/admin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： mysql
--- 生成日期： 2020-02-17 15:43:00
+-- 生成日期： 2020-04-16 19:31:01
 -- 服务器版本： 5.7.29
 -- PHP 版本： 7.4.1
 
@@ -28,19 +28,20 @@ SET time_zone = "+00:00";
 
 INSERT INTO `admin_menu` (`id`, `parent_id`, `order`, `title`, `icon`, `uri`, `permission`, `created_at`, `updated_at`) VALUES
 (1, 0, 1, '首页', 'fa-bar-chart', '/', NULL, NULL, '2020-02-07 05:38:40'),
-(2, 0, 9, '系统管理', 'fa-tasks', NULL, NULL, NULL, '2020-02-17 15:12:34'),
-(3, 2, 10, '管理员', 'fa-users', 'auth/users', NULL, NULL, '2020-02-17 15:12:34'),
-(4, 2, 11, '角色', 'fa-user', 'auth/roles', NULL, NULL, '2020-02-17 15:12:34'),
-(5, 2, 12, '权限', 'fa-ban', 'auth/permissions', NULL, NULL, '2020-02-17 15:12:35'),
-(6, 2, 13, '菜单', 'fa-bars', 'auth/menu', NULL, NULL, '2020-02-17 15:12:35'),
-(7, 2, 14, '操作日志', 'fa-history', 'auth/logs', NULL, NULL, '2020-02-17 15:12:35'),
+(2, 0, 10, '系统管理', 'fa-tasks', NULL, NULL, NULL, '2020-04-16 19:13:19'),
+(3, 2, 11, '管理员', 'fa-users', 'auth/users', NULL, NULL, '2020-04-16 19:13:19'),
+(4, 2, 12, '角色', 'fa-user', 'auth/roles', NULL, NULL, '2020-04-16 19:13:19'),
+(5, 2, 13, '权限', 'fa-ban', 'auth/permissions', NULL, NULL, '2020-04-16 19:13:19'),
+(6, 2, 14, '菜单', 'fa-bars', 'auth/menu', NULL, NULL, '2020-04-16 19:13:19'),
+(7, 2, 15, '操作日志', 'fa-history', 'auth/logs', NULL, NULL, '2020-04-16 19:13:19'),
 (8, 0, 2, '用户管理', 'fa-users', '/users', NULL, '2020-02-07 05:40:28', '2020-02-07 05:40:40'),
 (9, 0, 4, '商品管理', 'fa-cubes', '/products', NULL, '2020-02-07 05:51:07', '2020-02-17 05:29:08'),
-(10, 0, 7, '订单管理', 'fa-cny', '/orders', NULL, '2020-02-13 03:52:03', '2020-02-17 15:12:34'),
-(11, 0, 8, '优惠券管理', 'fa-tags', '/coupon_codes', NULL, '2020-02-14 19:41:04', '2020-02-17 15:12:34'),
+(10, 0, 8, '订单管理', 'fa-cny', '/orders', NULL, '2020-02-13 03:52:03', '2020-04-16 19:13:19'),
+(11, 0, 9, '优惠券管理', 'fa-tags', '/coupon_codes', NULL, '2020-02-14 19:41:04', '2020-04-16 19:13:19'),
 (12, 0, 3, '类目管理', 'fa-bars', '/categories', NULL, '2020-02-17 05:28:55', '2020-02-17 05:29:08'),
 (13, 9, 6, '众筹商品', 'fa-flag-checkered', '/crowdfunding_products', NULL, '2020-02-17 15:11:24', '2020-02-17 15:12:34'),
-(14, 9, 5, '普通商品', 'fa-cubes', '/products', NULL, '2020-02-17 15:12:17', '2020-02-17 15:12:34');
+(14, 9, 5, '普通商品', 'fa-cubes', '/products', NULL, '2020-02-17 15:12:17', '2020-02-17 15:12:34'),
+(15, 9, 7, '秒杀商品', 'fa-bolt', 'seckill_products', NULL, '2020-04-16 19:13:08', '2020-04-16 19:13:19');
 
 --
 -- 转存表中的数据 `admin_permissions`
@@ -99,7 +100,7 @@ INSERT INTO `admin_role_users` (`role_id`, `user_id`, `created_at`, `updated_at`
 --
 
 INSERT INTO `admin_users` (`id`, `username`, `password`, `name`, `avatar`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '$2y$10$pVcfNGeQyv9Yw3pQ/DtjfOxU6dPVZzZJCUnRo3qJSK1/GQHnGjGW6', 'Administrator', NULL, 'HqRr2CaGmzhTNEOW066wSB6sXFopNX8tGuC5l4eZNOU7uOWUZj5qvR7apgWr', '2020-02-07 05:34:35', '2020-02-07 05:34:35'),
+(1, 'admin', '$2y$10$pVcfNGeQyv9Yw3pQ/DtjfOxU6dPVZzZJCUnRo3qJSK1/GQHnGjGW6', 'Administrator', NULL, 'IQqshVQ9cLQN4rjR84DIEXS9w53Gg18YlcysDIgnBUT6VEXE9cywx4XAOJiv', '2020-02-07 05:34:35', '2020-02-07 05:34:35'),
 (2, 'operator', '$2y$10$BDLNVL3IlHxuU6VQ0/WTPuIixouVfRW2Us8duY18BQgkX6W3MEX2m', '运营', NULL, 'wYf4cUFPLO26q6n6TuW78fOxqLzSwprRPYGn4NpEBB5NARIRoEI6zunkxMyc', '2020-02-07 05:49:10', '2020-02-07 05:49:10');
 COMMIT;
 
